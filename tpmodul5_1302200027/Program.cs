f10502eb04f9ea8264fd62a60455803996940588
ARZzxz <achmadrizky0211@gmail.com>
2022-03-25 17:27:47 +0700

    Implementasi design by contract

--- tpmodul5_1302200027/Program.cs
+++ tpmodul5_1302200027/Program.cs
@@ -26,17 +26,28 @@ namespace tpmodul5_1302200027
 
             public SayaTubeVideo(string judul)
             {
-                
-                Random ids = new Random();
+                //contract no5
+            Contract.Requires(title != null);
+            Contract.Requires(title.Length < 100);
+            Random ids = new Random();
                 this.title = judul;
                 id = ids.Next(0, 100000);
                 this.playCount = 0;
             }
             public void IncreasePlayCount(int i)
             {
+                //trycatch no5
+                 try
+                {
+                    if (i >= 10000000) throw new Exception("angka melebihi batas");
+                     playCount = playCount + i;
+                }
+                catch (Exception e)
+                {
+                 Console.WriteLine(e.Message);
+                }
             playCount = playCount + i;
             }
-
             public void PrintVideoDetails()
             {
                 Console.WriteLine(this.id);
